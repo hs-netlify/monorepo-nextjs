0aac1434967c28a3b6ccba0df778398748e5236c
Henry Smith <henry.smith@netlify.com>
2022-05-20 11:05:38 +0100

    Remove submodules

---
+++ app1/next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/*",
+        headers: [
+          {
+            key: "x-custom-header-config",
+            value: "test1",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
